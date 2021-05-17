92dce9335376fc41b66193b1235880d4ccc50ef7
FerroEduardo <47820549+ferroeduardo@users.noreply.github.com>
2021-05-17 15:04:56 -0300

    adjust column width

--- main.js
+++ main.js
@@ -72,6 +72,13 @@ if(SUBJECTS != null) {
 
 //Estou na parte de se inscrever em disciplinas equivalentes
 if(EQUIVALENT_SUBJECTS != null) {
+  /*Se estiver na página de disciplinas equivalentes,
+    define o width da coluna de horário como 16%
+    para uma visualização melhor
+  */
+  if (EQUIVALENT_SUBJECTS.length != 0) {
+    document.getElementById('lista-turmas-curriculo').firstElementChild.firstElementChild.children[4].setAttribute('width', '16%');
+  }
   for(let i = 0; i<EQUIVALENT_SUBJECTS.length; i++) {
     /*Para cada linha de disciplina (EQUIVALENT_SUBJECTS[i]), 
       seleciona a coluna que contém o código de horário (children[6]),
@@ -86,6 +93,13 @@ if(EQUIVALENT_SUBJECTS != null) {
 
 //Estou na parte de se inscrever em disciplinas abertas
 if(OPEN_SUBJECTS != null) {
+  /*Se estiver na página de disciplinas abertas,
+    define o width da coluna de horário como 18%
+    para uma visualização melhor
+  */
+  if (OPEN_SUBJECTS.length != 0) {
+    document.getElementById('lista-turmas-extra').firstElementChild.firstElementChild.children[5].setAttribute('width', '18%');
+  }
   for(let i = 0; i<OPEN_SUBJECTS.length; i++) {
     /*Para cada linha de disciplina (OPEN_SUBJECTS[i]), 
       seleciona a coluna que contém o código de horário (children[7]),
